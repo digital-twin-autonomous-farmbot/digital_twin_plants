61fd50c36897009cfd5b4a7ab3515fc4c732d5db
grahanoi <grahanoi@students.zhaw.ch>
2025-06-02 16:30:27 +0200

    print out the information of the bounding boxes in a file

--- take_plant_picture.sh
+++ take_plant_picture.sh
@@ -12,13 +12,13 @@ for i in $(seq -f "%02g" 1 15); do
     # Capture with AI camera (right)
     libcamera-jpeg --camera 1 -o $OUTDIR/right_plant$i.jpg --width 640 --height 480
 
-    # Run AI detection on the right image and save bounding box
+    # Run AI detection on the right image and save bounding box output to text file
     rpicam-hello \
       --timeout 1s \
       --post-process-file /usr/share/rpi-camera-assets/imx500_mobilenet_ssd.json \
-      --post-process-output $OUTDIR/bbox_plant$i.json \
-      --input $OUTDIR/right_plant$i.jpg \
-      --width 640 --height 480
+      --width 640 --height 480 \
+      --output /dev/null \
+      > $OUTDIR/bbox_plant$i.txt
 
     echo "Bildpaar $i aufgenommen und Bounding Box gespeichert"
     sleep 2
