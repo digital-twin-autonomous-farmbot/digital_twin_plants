866b0f4ff7df0a11e7aa896e5906de1c7ae1b3e4
grahanoi <grahanoi@students.zhaw.ch>
2025-06-03 15:39:08 +0200

    add mean_depth sort counter

--- death_calculation/sorte_results.py
+++ death_calculation/sorte_results.py
@@ -6,18 +6,48 @@ with open("results/tiefenberechnung_results.yaml", "r") as f:
 # Define bins: (label, lower_bound, upper_bound)
 bins = [
     ("< 5 cm", 0, 5),
-    ("5–15 cm", 5, 15),
-    ("15–30 cm", 15, 30),
-    ("30–50 cm", 30, 50),
-    (">= 50 cm", 50, float("inf")),
+    ("6–15 cm", 6, 15),
+    ("16–30 cm", 16, 30),
+    ("31–50 cm", 31, 50),
+    (">= 51 cm", 51, float("inf")),
 ]
 
+# histogram teifenberechnung_results.yaml
+# Initialize counts for each bin
+bins_depth = [
+    ("< 50 cm", 0, 50),
+    ("51-75 cm", 51, 75),
+    ("75-100 cm", 76, 100),
+    ("101-125 cm", 101, 125),
+    ("126-150 cm", 126, 150),
+    (">= 151 cm", 151, float("inf")),
+]
 
+# histogram plant height
+  
 # Initialize counters
 counts = {label: 0 for (label, _, _) in bins}
 
 for entry in results:
     h = entry.get("plant_height_cm")
+    if h is not None and isinstance(h, (int, float)) and h == h:  # check for NaN
+        for label, low, high in bins:
+            if low <= h < high:
+                counts[label] += 1
+                break
+        else:
+            if h >= 50:
+                counts[">= 50 cm"] += 1
+
+print("Gruppierte Pflanzenhöhen:")
+for label in counts:
+    print(f"{label}: {counts[label]}")
+
+# Initialize counters
+counts = {label: 0 for (label, _, _) in bins_depth}
+
+for entry in results:
+    h = entry.get("mean_depth")
     if h is not None and isinstance(h, (int, float)) and h == h:  # check for NaN
         for label, low, high in bins:
             if low <= h < high:
